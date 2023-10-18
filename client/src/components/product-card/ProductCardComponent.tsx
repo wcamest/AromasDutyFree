27a1d11178f53eef8545a7035e90629dd57dfcce
William Estrada <willkmiloeo1228@gmail.com>
2023-10-17 20:49:37 -0500

    fix.: very large texts in the product card component

--- client/src/components/product-card/ProductCardComponent.tsx
+++ client/src/components/product-card/ProductCardComponent.tsx
@@ -7,11 +7,11 @@ import ButtonComponent from "../controls/button/ButtonComponent";
 
 type Props = {
   data: Product;
-  maxWidth: number;
+  maxWidth?: number;
 };
 
 const ProductCardComponent = (props: Props) => {
-  const { data } = props;
+  const { data, maxWidth } = props;
 
   const Renderer = {
     FeaturedImage() {
@@ -34,16 +34,16 @@ const ProductCardComponent = (props: Props) => {
 
   return (
     <div
-      style={{ maxWidth: `${props.maxWidth}px` }}
-      className="p-2 sm:p-4 flex flex-col gap-1 border border-solid border-rose-300 rounded-md"
+      style={{ maxWidth: maxWidth ? `${props.maxWidth}px` : undefined }}
+      className="p-4 flex flex-col gap-1 border border-solid border-rose-300 rounded-md"
     >
       <div className="w-full relative aspect-square border border-solid border-rose-300 overflow-hidden rounded-md">
         {Renderer.FeaturedImage()}
       </div>
-      <h2 className="text-lg sm:text-xl">{data.name}</h2>
-      <h3 className="text-xs sm:text-lg">{`$${Utils.FormatCurrency(data.salePrice)}`}</h3>
+      <h2 className="text-lg md:text-xl">{data.name}</h2>
+      <h3 className="text-xs md:text-lg">{`$${Utils.FormatCurrency(data.salePrice)}`}</h3>
       <div className="w-full mt-8 flex justify-stretch">
-        <ButtonComponent className="w-full text-xs sm:text-lg">Consultar stock</ButtonComponent>
+        <ButtonComponent className="w-full text-xs md:text-lg">Consultar stock</ButtonComponent>
       </div>
     </div>
   );
